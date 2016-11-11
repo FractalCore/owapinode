062297586aaca77add9f89d5400d1d3f3e8bdf5d
Quinton Marchi <quinton@fractalcore.net>
2016-11-11 10:59:14 -0600

    Update for the parsing errors in #2, and updated for new data in #3
    Added additional fields for data, probably going to need to break it
    out for better filtering.
    
    Probably should do better filtering on the data fields to better deal
    with PlayOverwatch changes….

--- routes/user.js
+++ routes/user.js
@@ -27,24 +27,78 @@ router.get('/:username', function(req, res) {
 			
 			//Load the page into cheerio
 			var $ = cheerio.load(html);
-			
+
+			//Could do this in the JSON Builder, don't want to...
+			var pDeath = parseInt($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(5)>div>table>tbody>tr:nth-child(1)>td:nth-child(2)').text().replace(/,/g,'')) - parseInt($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(5)>div>table>tbody>tr:nth-child(2)>td:nth-child(2)').text().replace(/,/g,''));
+
 			//Build the JSON Response
 			var json = {
 				battlenetID: username,
-				level:		$('div.u-vertical-center').text(),
-				games:		$('section#stats-section>div>div:nth-child(4)>div:nth-child(1)>div:nth-child(7)>div>table>tbody>tr:nth-child(2)>td:nth-child(2)').text(),
-				gamesWon:	$('section#stats-section>div>div:nth-child(4)>div:nth-child(1)>div:nth-child(7)>div>table>tbody>tr:nth-child(1)>td:nth-child(2)').text(),
+				level:		$('div.show-for-lg>div:nth-child(1)>div').text(),
 				averages: {
-					eliminations:	$('section#highlights-section>div>ul>li:nth-child(1)>div>div>h3').text(),
-					damage:			$('section#highlights-section>div>ul>li:nth-child(2)>div>div>h3').text(),
-					deaths:			$('section#highlights-section>div>ul>li:nth-child(3)>div>div>h3').text(),
-					finalBlows:		$('section#highlights-section>div>ul>li:nth-child(4)>div>div>h3').text(),
-					healing:		$('section#highlights-section>div>ul>li:nth-child(5)>div>div>h3').text(),
-					objKills:		$('section#highlights-section>div>ul>li:nth-child(6)>div>div>h3').text(),
-					objTime:		$('section#highlights-section>div>ul>li:nth-child(7)>div>div>h3').text(),
-					soloKills:		$('section#highlights-section>div>ul>li:nth-child(8)>div>div>h3').text()
+					timeOnFire:		$('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(4)>div>table>tbody>tr:nth-child(1)>td:nth-child(2)').text(),
+					soloKills:		parseFloat($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(4)>div>table>tbody>tr:nth-child(2)>td:nth-child(2)').text().replace(/,/g,'')),
+					objTime:		parseFloat($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(4)>div>table>tbody>tr:nth-child(3)>td:nth-child(2)').text().replace(/,/g,'')),
+					objKills:		parseFloat($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(4)>div>table>tbody>tr:nth-child(4)>td:nth-child(2)').text().replace(/,/g,'')),
+					healing:		parseFloat($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(4)>div>table>tbody>tr:nth-child(5)>td:nth-child(2)').text().replace(/,/g,'')),
+					finalBlows:		parseFloat($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(4)>div>table>tbody>tr:nth-child(6)>td:nth-child(2)').text().replace(/,/g,'')),
+					deaths:			parseFloat($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(4)>div>table>tbody>tr:nth-child(7)>td:nth-child(2)').text().replace(/,/g,'')),
+					damage:			parseFloat($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(4)>div>table>tbody>tr:nth-child(8)>td:nth-child(2)').text().replace(/,/g,'')),
+					eliminations:	parseFloat($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(4)>div>table>tbody>tr:nth-child(9)>td:nth-child(2)').text().replace(/,/g,''))
+				},
+				totals: {
+					combat: {
+						soloKills: 			parseInt($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(1)>div>table>tbody>tr:nth-child(1)>td:nth-child(2)').text().replace(/,/g,'')),
+						objectiveKills:		parseInt($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(1)>div>table>tbody>tr:nth-child(2)>td:nth-child(2)').text().replace(/,/g,'')),
+						finalBlows:			parseInt($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(1)>div>table>tbody>tr:nth-child(3)>td:nth-child(2)').text().replace(/,/g,'')),
+						damageDone:			parseInt($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(1)>div>table>tbody>tr:nth-child(4)>td:nth-child(2)').text().replace(/,/g,'')),
+						eliminations:		parseInt($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(1)>div>table>tbody>tr:nth-child(5)>td:nth-child(2)').text().replace(/,/g,'')),
+						environKills:		parseInt($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(1)>div>table>tbody>tr:nth-child(6)>td:nth-child(2)').text().replace(/,/g,'')),
+						multiKills:			parseInt($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(1)>div>table>tbody>tr:nth-child(7)>td:nth-child(2)').text().replace(/,/g,''))
+					},
+					deaths: {
+						allDeaths:			parseInt($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(5)>div>table>tbody>tr:nth-child(1)>td:nth-child(2)').text().replace(/,/g,'')),
+						envDeaths:			parseInt($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(5)>div>table>tbody>tr:nth-child(2)>td:nth-child(2)').text().replace(/,/g,'')),
+						playerDeaths:		pDeath
+					},
+					awards: {
+						cards:				parseInt($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(6)>div>table>tbody>tr:nth-child(1)>td:nth-child(2)').text().replace(/,/g,'')),
+						totalMedals:		parseInt($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(6)>div>table>tbody>tr:nth-child(2)>td:nth-child(2)').text().replace(/,/g,'')),
+						goldMedals:			parseInt($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(6)>div>table>tbody>tr:nth-child(3)>td:nth-child(2)').text().replace(/,/g,'')),
+						silverMedals:		parseInt($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(6)>div>table>tbody>tr:nth-child(4)>td:nth-child(2)').text().replace(/,/g,'')),
+						bronzeMedals:		parseInt($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(6)>div>table>tbody>tr:nth-child(5)>td:nth-child(2)').text().replace(/,/g,''))
+					},
+					assists: {
+						healing:			parseInt($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(2)>div>table>tbody>tr:nth-child(1)>td:nth-child(2)').text().replace(/,/g,'')),
+						recon:				parseInt($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(2)>div>table>tbody>tr:nth-child(2)>td:nth-child(2)').text().replace(/,/g,''))
+					},
+					misc: {
+						defensiveAssists:	parseInt($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(8)>div>table>tbody>tr:nth-child(1)>td:nth-child(2)').text().replace(/,/g,'')),
+						defensiveAssistAvg:	parseFloat($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(8)>div>table>tbody>tr:nth-child(2)>td:nth-child(2)').text().replace(/,/g,'')),
+						offensiveAssists:	parseInt($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(8)>div>table>tbody>tr:nth-child(3)>td:nth-child(2)').text().replace(/,/g,'')),
+						offensiveAssistAvg: parseFloat($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(8)>div>table>tbody>tr:nth-child(4)>td:nth-child(2)').text().replace(/,/g,''))
+					},
+					best: {
+						eliminations:		parseInt($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(3)>div>table>tbody>tr:nth-child(1)>td:nth-child(2)').text()),
+						finalBlows:			parseInt($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(3)>div>table>tbody>tr:nth-child(2)>td:nth-child(2)').text()),
+						damageDone:			parseInt($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(3)>div>table>tbody>tr:nth-child(3)>td:nth-child(2)').text()),
+						healingDone:		parseInt($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(3)>div>table>tbody>tr:nth-child(4)>td:nth-child(2)').text()),
+						defensiveAssists:	parseInt($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(3)>div>table>tbody>tr:nth-child(5)>td:nth-child(2)').text()),
+						offensiveAssists:	parseInt($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(3)>div>table>tbody>tr:nth-child(6)>td:nth-child(2)').text()),
+						objectiveKills:		parseInt($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(3)>div>table>tbody>tr:nth-child(7)>td:nth-child(2)').text()),
+						objectiveTime:		$('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(3)>div>table>tbody>tr:nth-child(8)>td:nth-child(2)').text(),
+						mutiKill:			parseInt($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(3)>div>table>tbody>tr:nth-child(9)>td:nth-child(2)').text()),
+						soloKill:			parseInt($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(3)>div>table>tbody>tr:nth-child(10)>td:nth-child(2)').text()),
+						timeOnFire:			$('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(3)>div>table>tbody>tr:nth-child(11)>td:nth-child(2)').text()
+					},
+					games: {
+						gamesWon:			parseInt($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(7)>div>table>tbody>tr:nth-child(1)>td:nth-child(2)').text()),
+						timeOnFire:			$('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(7)>div>table>tbody>tr:nth-child(2)>td:nth-child(2)').text(),
+						objectiveTime:		$('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(7)>div>table>tbody>tr:nth-child(3)>td:nth-child(2)').text(),
+						timePlayed:			parseInt($('div#quickplay>section:nth-child(3)>div>div:nth-child(3)>div:nth-child(7)>div>table>tbody>tr:nth-child(4)>td:nth-child(2)').text().replace(' hours',''))
+					}
 				}
-			}
+			};
 			
 			res.send(json);
 			
