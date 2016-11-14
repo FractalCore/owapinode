749040238e08d5590879538471729c1c14995da6
Quinton Marchi <quinton@fractalcore.net>
2016-11-14 11:47:22 -0600

    Finish and close #5, currently nothing special
    Going to split up data.

--- routes/v2/user.js
+++ routes/v2/user.js
@@ -2,24 +2,14 @@ var restifyRouter	= require('restify-routing'),
     router          = new restifyRouter(),
 	user			= require(process.cwd() + '/data/user.js');
 
-router.get('/:region:/:platform:/:username:', function(req, res) {
-
-	//Pull Username from req
-	var platform	= req.params.platform,
-		region		= req.params.region,
-		username	= req.params.username;
+router.get('/:region/:platform/:username', function(req, res) {
 
 	console.log(req.params);
 
-	var successRes	= function(json){
-		res.send(json);
-	};
-
-	var errorRes	= function(){
-		res.send(500, "Internal Server Error");
-	}
-
-	var json = user(platform, username, region, successRes, errorRes);
+	user(req.params.platform, req.params.username, req.params.region,
+		function(json) { res.send(json); },
+		function() { res.send(500, "Internal Server Error" ); }
+	);
 
 
 
